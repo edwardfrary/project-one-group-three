ac721eba67536fb47327c5e741494b05640b3abc
edwardfrary <edwardfrary@gmail.com>
2021-05-22 07:02:31 -0400

    delete functionality

--- assets/javascript/jscript.js
+++ assets/javascript/jscript.js
@@ -14,21 +14,29 @@ $("#tasksubmit").on("click", function () {
   //Object creation to hold the inputted values
   var taskObj = {
     title: taskTitle,
-    body: taskBody
+    body: taskBody,
   };
-
+ 
   //load previous task array, push the new object into it then save the array into local storage
-  taskArr = loadTask()
-  renderTasks(taskArr);
+  renderTasks(taskObj);
   saveTask(taskObj);
   
+  //reset the modals text boxes
+ $(".modal-text-title")
+ .val("");
+
+ $(".modal-text-body")
+ .val("");
+
 });
 
-//Function to render the cards using the inputs from the modal text boxes
+//Handler for the delete tasks button
+$(document).on("click", ".delete-button", function () {
+  $(this).closest(".cell").remove();
+});
 
+//Function to render the cards using the input from either the modal text boxes OR from local storage memory
 function renderTasks(taskObj) {
-
-
   //create the card container and then the other elements of the card
   var cardCellEl = $("<div>")
     .addClass("cell");
@@ -36,27 +44,44 @@ function renderTasks(taskObj) {
     .addClass("card");
   var cardHeaderEl = $("<div>")
     .addClass("card-divider")
-    .text(taskObj[i].title);
+    .text(taskObj.title);
+  var cardDeleteEl = $("<a>")
+    .addClass("delete-button")
+    .attr("style", "color:red;")
+    .html("<i class='material-icons'>clear</i>");
   var cardBodyEl = $("<div>")
     .addClass("card-section")
-    .text(taskObj[i].body);
+    .text(taskObj.body);
 
   $(cardContainerEl).append(cardHeaderEl);
+  $(cardHeaderEl).append(cardDeleteEl);
   $(cardContainerEl).append(cardBodyEl);
   $(cardCellEl).append(cardContainerEl);
   $("#noticeboard-body").append(cardCellEl);
 };
 
 function saveTask(taskObj) {
+
   //first load up the previous data
-  dataArr = JSON.parse(localStorage.getItem("data"));
+  var dataArr = JSON.parse(localStorage.getItem("data"));
   if (!dataArr) {
     dataArr = [];
   };
   //push in the new data and then save it to local storage
   dataArr.push(taskObj);
   localStorage.setItem("data", JSON.stringify(dataArr));
-
-  //load all the previously saved tasks and render them to the browser
-
 };
+
+function loadTask() {
+  //load the previously saved tasks
+  var dataArr = JSON.parse(localStorage.getItem("data"));
+  if (!dataArr) {
+    dataArr = [];
+  };
+  //for loop to render each object, increment the tracer by 1 to give each card a unique ID
+  for (i = 0; i < dataArr.length; i++) {
+    renderTasks(dataArr[i]);
+  };
+};
+
+loadTask();
